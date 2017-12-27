e3ca05b080f7083f1f69f8913782a0d0d1ba59fd
abhijitkulkarni20 <34384839+abhijitkulkarni20@users.noreply.github.com>
2017-12-26 22:57:13 -0600

    Update SeriesLogic.java

--- 2861/SeriesLogic.java
+++ 2861/SeriesLogic.java
@@ -2,7 +2,7 @@ package com.htc.classexercise;
 
 public class SeriesLogic {
 	 public static void main(String args[]) {
-		 int i = 100;  //numbers to be printed in series
+		 int i = 99;  //Max number to be printed in series
 		 int cntr = 1; // temp variable to flip sign
 		 for (i=1;i<100;i++) {
 			 if(i % 2 == 0) {
